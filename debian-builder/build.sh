f04f2cdd9f561e0ca5941682fd14a7fe79741c74
Enrico204 <enrico204@gmail.com>
2022-09-07 11:40:14 +0200

    Better build script for debian-builder

--- debian-builder/build.sh
+++ debian-builder/build.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 VERSION=0
 
-for distro in stretch; do # buster bullseye; do
+for distro in stretch buster bullseye; do
 	IMAGE_PATH="$BASEURL/debian-builder:$distro-v$VERSION"
 	DOCKERHUB_IMAGE_PATH="$DOCKERHUB_BASEURL/debian-builder:$distro-v$VERSION"
 
@@ -11,7 +11,7 @@ for distro in stretch; do # buster bullseye; do
 	if [ $? -ne 0 ]; then
 		set -e
 		# Image does not exists
-		buildah manifest rm containers-storage:$IMAGE_PATH || true > /dev/null
+		buildah manifest rm containers-storage:$IMAGE_PATH || true > /dev/null 2>&1
 		buildah manifest create $IMAGE_PATH
 
 		BUILDAH_ARGS="--build-arg apt_proxy=$apt_proxy -f Dockerfile.$distro --manifest $IMAGE_PATH"
@@ -19,11 +19,21 @@ for distro in stretch; do # buster bullseye; do
 		buildah bud ${BUILDAH_ARGS} --arch arm64 --variant v8
 		buildah bud ${BUILDAH_ARGS} --arch arm   --variant v7
 
-		# ARMel specific
+		buildah manifest push --all --format=docker $IMAGE_PATH docker://$IMAGE_PATH
+		set +e
+	fi
+
+	# ARMel specific
+	skopeo --override-arch armel --override-variant v7 inspect "docker://$IMAGE_PATH-armel" > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		set -e
+		# Image does not exists
+		buildah manifest rm containers-storage:$IMAGE_PATH-armel || true > /dev/null 2>&1
+		buildah manifest create $IMAGE_PATH-armel
+
 		BUILDAH_ARGS="--build-arg apt_proxy=$apt_proxy -f Dockerfile.$distro.armel --manifest $IMAGE_PATH-armel"
 		buildah bud ${BUILDAH_ARGS} --arch arm   --variant v7
 
-		buildah manifest push --all --format=docker $IMAGE_PATH docker://$IMAGE_PATH
 		buildah manifest push --all --format=docker $IMAGE_PATH-armel docker://$IMAGE_PATH-armel
 		set +e
 	fi
@@ -33,6 +43,13 @@ for distro in stretch; do # buster bullseye; do
 	if [ $? -ne 0 ]; then
 		set -e
 		buildah manifest push --all --format=docker $IMAGE_PATH "docker://$DOCKERHUB_IMAGE_PATH"
+		set +e
+	fi
+
+	# Mirror container in Docker Hub (ARMel version)
+	skopeo --override-arch armel --override-variant v7 inspect "docker://$DOCKERHUB_IMAGE_PATH-armel" > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		set -e
 		buildah manifest push --all --format=docker $IMAGE_PATH-armel "docker://$DOCKERHUB_IMAGE_PATH-armel"
 		set +e
 	fi
