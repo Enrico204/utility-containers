6aeb328294b02db25a645406e5fe1318fcef3ab5
Enrico204 <enrico204@gmail.com>
2023-05-31 00:22:08 +0200

    mediamtx

--- update_dockerhub.sh
+++ update_dockerhub.sh
@@ -3,7 +3,7 @@ set -xeou pipefail
 
 # This script updates the description for each page on DockerHub
 
-for tag in adminer android-lint android-sdk argocd buildah-builder eslint ffvnc flutter-sdk go-tensorflow-lite golang hugo-netlify hugo-pandoc mariadb-dbmate openapi platformio postgres-dbmate qbittorrent; do
+for tag in adminer android-lint android-sdk argocd buildah-builder eslint ffvnc flutter-sdk go-tensorflow-lite golang hugo-netlify hugo-pandoc mariadb-dbmate openapi platformio postgres-dbmate qbittorrent mediamtx; do
     podman run -it --rm --user 0 \
         -v "$HOME/.docker/config.json:/config.json:ro" \
         -v "$(pwd):/src/:ro" \
