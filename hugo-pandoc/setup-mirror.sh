44197b3ff13ea5a97a396f0a6225957864edb261
Enrico204 <enrico204@gmail.com>
2023-10-29 07:36:01 +0100

    Fix setup mirror on hugo-pandoc

--- hugo-pandoc/setup-mirror.sh
+++ hugo-pandoc/setup-mirror.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+. /etc/os-release
+
 rm -f /etc/apt/sources.list
 
 if [ "${1:-}" == "sysprep" ]; then
@@ -15,17 +17,17 @@ else
 
     if [ "${apt_mirror:-}" != "" ]; then
         cat > /etc/apt/sources.list <<EOF
-deb $apt_mirror/debian/ bullseye main contrib non-free
-deb $apt_mirror/debian-security bullseye-security main contrib non-free
-deb $apt_mirror/debian/ bullseye-updates main contrib non-free
-#deb $apt_mirror/debian/ bullseye-backports main contrib non-free
+deb $apt_mirror/debian/ $VERSION_CODENAME main contrib non-free
+deb $apt_mirror/debian-security $VERSION_CODENAME-security main contrib non-free
+deb $apt_mirror/debian/ $VERSION_CODENAME-updates main contrib non-free
+#deb $apt_mirror/debian/ $VERSION_CODENAME-backports main contrib non-free
 EOF
     fi
 fi
 
 cat >> /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian/ bullseye main non-free contrib
-deb http://security.debian.org/debian-security bullseye-security main contrib non-free
-deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-#deb http://deb.debian.org/debian/ bullseye-backports main contrib non-free
+deb http://deb.debian.org/debian/ $VERSION_CODENAME main non-free contrib
+deb http://security.debian.org/debian-security $VERSION_CODENAME-security main contrib non-free
+deb http://deb.debian.org/debian/ $VERSION_CODENAME-updates main contrib non-free
+#deb http://deb.debian.org/debian/ $VERSION_CODENAME-backports main contrib non-free
 EOF
