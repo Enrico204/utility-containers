0fab57be613fee4fc4d8f7948ed5a871feca6e39
Enrico204 <enrico204@gmail.com>
2023-10-29 08:36:00 +0100

    hugo-pandoc: use correct versions from repo

--- update_versions.sh
+++ update_versions.sh
@@ -16,25 +16,30 @@ github_tag() {
     curl -s "https://api.github.com/repos/$1/git/refs/tags" | jq -r ".[].ref" | sed 's/^refs\/tags\///' | tr -d 'v' | grep -vE 'pre$' | tail -n 1
 }
 
+netsplit_repo_version() {
+    curl -s "https://deb.netsplit.it/$1/Packages" | grep -A5 "Package: $2" | awk -v arch="$3" -v pkgarch="" '
+/^Architecture:/ { pkgarch = $2 }
+/^Version:/ { if (pkgarch == arch) print $2; pkgarch = "" }
+' | grep -vE '^$' | sort -V | tail -n 1
+}
+
+
 docker_version quay.io/buildah/stable > buildah-builder/BUILDAH.version
 
 github_tag flutter/flutter > flutter-sdk/FLUTTER.version
 
 docker_version docker.io/library/golang > golang/GO.version
-#cp golang/GO.version go-tensorflow-lite/GO.version
 
-github_version gohugoio/hugo > hugo-pandoc/HUGO.version
+netsplit_repo_version selfcontained hugo amd64 > hugo-pandoc/HUGO.version
+#github_version gohugoio/hugo > hugo-pandoc/HUGO.version
 #cp hugo-pandoc/HUGO.version hugo-netlify/HUGO.version
 
-github_version jgm/pandoc > hugo-pandoc/PANDOC.version
-github_version plantuml/plantuml > hugo-pandoc/PLANTUML.version
-
-github_version amacneil/dbmate > mariadb-dbmate/DBMATE.version
-docker_version docker.io/library/mariadb > mariadb-dbmate/MARIADB.version
+#github_version jgm/pandoc > hugo-pandoc/PANDOC.version
+netsplit_repo_version selfcontained pandoc amd64 > hugo-pandoc/PANDOC.version
+#github_version plantuml/plantuml > hugo-pandoc/PLANTUML.version
+netsplit_repo_version selfcontained plantuml all > hugo-pandoc/PLANTUML.version
 
 docker_version docker.io/openapitools/openapi-generator-cli > openapi/OPENAPI.version
 
-cp mariadb-dbmate/DBMATE.version postgres-dbmate/DBMATE.version
+github_version amacneil/dbmate > postgres-dbmate/DBMATE.version
 docker_version docker.io/library/postgres > postgres-dbmate/POSTGRES.version
-
-#cp mariadb-dbmate/MARIADB.version mariadb-ldap/MARIADB.version
