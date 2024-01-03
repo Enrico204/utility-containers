2293aad419a0c43fc22aef4940c8a6ec673faf43
Enrico204 <enrico204@gmail.com>
2024-01-03 08:23:47 +0100

    Add nodered, novnc, update flutter-sdk, postgres-dbmate

--- build_images.sh
+++ build_images.sh
@@ -11,7 +11,7 @@ if ! command -v skopeo; then
     exit 1
 fi
 
-IMAGES="android-lint android-sdk aria2c aria2c-webui buildah-builder eslint ffvnc flutter-sdk golang mediamtx openapi platformio postgres-dbmate qbittorrent rsyslog"
+IMAGES="android-lint android-sdk aria2c aria2c-webui buildah-builder eslint ffvnc flutter-sdk golang mediamtx node-red novnc openapi platformio postgres-dbmate qbittorrent rsyslog"
 
 if [ "$1" != "" ]; then
     IMAGES="$1"
