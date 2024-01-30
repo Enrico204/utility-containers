b360fceb51ec02ca9e8265117f30a0834259a5fe
Enrico204 <enrico204@gmail.com>
2024-01-31 00:20:47 +0100

    asterisk: do not copy sounds every time

--- asterisk/init.sh
+++ asterisk/init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-cp -r /var/lib/asterisk-master/* /var/lib/asterisk/
+if [ ! -d /var/lib/asterisk/sounds ]; then
+    cp -r /var/lib/asterisk-master/* /var/lib/asterisk/
+fi
 
 if [ ! -e /etc/asterisk/modules.conf ]; then
     cp -r /etc/asterisk-master/* /etc/asterisk/
