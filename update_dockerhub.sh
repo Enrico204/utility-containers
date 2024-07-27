943f62aa9de50c6a12aa468044b893531c75c8bc
Enrico204 <enrico204@gmail.com>
2024-07-27 13:20:47 +0200

    Copy images in DockerHub and update README in one script

--- update_dockerhub.sh
+++ update_dockerhub.sh
@@ -5,9 +5,18 @@ set -xeou pipefail
 
 # This script updates the description for each page on DockerHub
 
-for tag in $IMAGES; do
+for image in $IMAGES; do
+    cd "$image"
+    VERSION=$(make version)
+    
+    if ! skopeo inspect "docker://$DOCKERHUB_BASEURL/$image:$VERSION" > /dev/null 2>&1; then
+        skopeo copy -a "docker://$BASEURL/$image:$VERSION" "docker://$DOCKERHUB_BASEURL/$image:$VERSION"
+    fi
+
     podman run -it --rm --user 0 \
         -v "${XDG_RUNTIME_DIR:-/var/tmp/containers-user-$UID/containers}/containers/auth.json:/config.json:ro" \
-        -v "$(pwd):/src/:ro" \
-        docker.io/chko/docker-pushrm "docker.io/enrico204/$tag" -f /src/README.md --config /config.json
+        -v "$(pwd)/../:/src/:ro" \
+        docker.io/chko/docker-pushrm "$DOCKERHUB_BASEURL/$image" -f /src/README.md --config /config.json
+    
+    cd ..
 done
