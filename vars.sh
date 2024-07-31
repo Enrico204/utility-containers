61b83ed31848e8d0d5aa8cb4a56e7f326a8891e4
Enrico204 <enrico204@gmail.com>
2024-07-31 23:50:49 +0200

    Add Android SDK image

--- vars.sh
+++ vars.sh
@@ -2,4 +2,4 @@
 
 export BASEURL="hub.netsplit.it/utilities"
 export DOCKERHUB_BASEURL="docker.io/enrico204"
-export IMAGES="aria2c aria2c-webui asterisk buildah-builder dmarc-analyzer dnsmasq eslint ffvnc golang mediamtx node-red openapi platformio postgres-dbmate qbittorrent rsyslog vscode-server-ipython"
+export IMAGES="android-sdk aria2c aria2c-webui asterisk buildah-builder dmarc-analyzer dnsmasq eslint ffvnc golang mediamtx node-red openapi platformio postgres-dbmate qbittorrent rsyslog vscode-server-ipython"
