c8a21de49eea79cb374b20ab025a1cd1ae850287
Enrico204 <enrico204@gmail.com>
2024-12-08 21:44:58 +0100

    remove mediamtx, update versions

--- update_versions.sh
+++ update_versions.sh
@@ -41,7 +41,7 @@ netsplit_repo_version selfcontained hugo amd64 > hugo/HUGO.version
 
 docker_version docker.io/openapitools/openapi-generator-cli > openapi/OPENAPI.version
 
-github_version bluenviron/mediamtx > mediamtx/MEDIAMTX.version
+#github_version bluenviron/mediamtx > mediamtx/MEDIAMTX.version
 
 github_version amacneil/dbmate > postgres-dbmate/DBMATE.version
 docker_version docker.io/library/postgres > postgres-dbmate/POSTGRES.version
